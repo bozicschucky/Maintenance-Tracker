f2b667922aeaba7dd3af7633eaef7cde5cbf3ca9
Ssekitto charlse <bozicslxye1@gmail.com>
2018-05-29 19:03:11 -0700

    fix pep8 errors

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 #!flask/bin/python
-from flask import Flask, abort, request, url_for
-from flask.json import jsonify
+from flask import Flask
 
 app = Flask(__name__)
-
 
 
 @app.route('/api/v1/request/<int:id>', methods=['GET'])
 def get_user_request_by_id(id):
     pass
+
 
 @app.route('/api/v1/requests', methods=['GET'])
 def get_user_requests():
@@ -19,10 +18,10 @@ def get_user_requests():
 def create_user_request():
     pass
 
+
 @app.route('/api/v1/request/<int:id>', methods=['PUT'])
 def update_user_request(id):
     pass
-
 
 
 if __name__ == '__main__':
