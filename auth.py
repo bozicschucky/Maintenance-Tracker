a8cbb5cb7ff0879a6fc51d077e3e8acc44e66d9a
Ssekitto charlse <bozicslxye1@gmail.com>
2018-05-31 12:56:37 -0700

    add more users to test with

--- auth.py
+++ auth.py
@@ -1,7 +1,13 @@
 from user import User
 
 users = {
-    User(1, 'admin', '123456')
+    User(1, 'admin', '123456'),
+    User(2, 'charles', '1234'),
+    User(3, 'bes', '1234'),
+    User(4, 'ivan', '1234'),
+    User(5, 'mary', '1234'),
+    User(6, 'ruganda', '1234'),
+    User(7, 'johnz', '1234')
 }
 
 username_mapping = {u.username: u for u in users}
