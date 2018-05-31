496d344f5dd5c405cb447623048f9306a96b20f8
Ssekitto charlse <bozicslxye1@gmail.com>
2018-05-30 17:25:02 -0700

    add endpoints to the app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
 
 app = Flask(__name__)
 app.secret_key = 'Code'
 api = Api(app)
+
+api.add_resource(UserRequest, '/api/v1/user/request/<int:id>')
+api.add_resource(UserRequests, '/api/v1/user/requests')
+
+if __name__ == '__main__':
+    app.run(debug=True)
