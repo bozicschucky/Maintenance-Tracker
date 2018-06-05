a06e6418490fea2d402551c60a72eb40e9a1698f
Ssekitto charlse <bozicslxye1@gmail.com>
2018-06-04 18:32:45 -0700

    [chore #158059370] Fix the api tests

--- Api/tests/test_user_api.py
+++ Api/tests/test_user_api.py
@@ -45,32 +45,3 @@ class AppTestCase(unittest.TestCase):
         self.assertNotIn(b'id', resp.data)
         self.assertIn(b'Message', resp.data)
         # assert resp.status_code, 202
-
-#
-# import pytest
-# import requests
-#
-#
-# @pytest.fixture()
-# def client(request):
-#     def teardown():
-#         pass
-#
-#     request.addfinalizer(teardown)
-#
-#
-# def test_get_all_user_requests():
-#     """ Test api to get all the  requests created by the users """
-#     response = requests.get('https://maintainencetrackerapi.herokuapp.com/api/v1/user/requests')
-#     assert response.status_code, 200
-#     assert response.headers['content-type'] == 'application/json'
-#     assert b'request' in response.content
-#     assert b'id' in response.content
-#
-#
-# def test_auth_user_request_without_authentication():
-#     """ if check if i can get user requests without logging in
-#      this should fail """
-#     response1 = requests.get('https://maintainencetrackerapi.herokuapp.com/api/v1/user/requests')
-#     assert response1.status_code, 401
-#     assert response1.headers['content-type'] == 'application/json'
